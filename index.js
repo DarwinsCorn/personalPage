438d734f8d49c765b518189ffe5e68089a9641e5
DarwinsCorn <dietrevich@gmail.com>
2023-07-16 14:54:46 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const nameBanner = document.querySelector("#name");
+const aboutLink = document.querySelector("#aboutLink");
+const about = document.querySelector("#about");
+const projectsLink = document.querySelector("#projectsLink");
+const projects = document.querySelector("#projects");
+const resumeLink = document.querySelector("#resumeLink");
+const resume = document.querySelector("#resume");
+const clickMe = document.querySelector(".click-me");
+const left = document.querySelector("#left_panel");
+const body = document.querySelector("body");
+const fontFormat = document.querySelector(".font-formatting");
+const a = document.querySelectorAll("a");
+const sections = document.querySelectorAll("section");
+
+let toggler = true;
+let activeSection = resume;
+let activeLink = resumeLink;
+
+function selectedLinkColor(ele) {
+    a.forEach(link => link.style.color = link.id === ele.id ? "black" : "white");
+    activeLink = ele;
+}
+
+function selectedOpacity(ele) {
+    sections.forEach(sect => sect.style.opacity = sect.id === ele.id ? 1 : 0);
+    activeSection = ele;
+}
+
+nameBanner.addEventListener('click', (evt) => { 
+    evt.preventDefault();
+    left.style.transform = toggler ? "rotate(0)" : "rotate(45deg)";
+    body.style.backgroundColor = toggler ? "#d7cdc1": "white";
+    fontFormat.style.color = toggler ? "white" : "black";
+    nameBanner.style.left = toggler ? "20%" : "50%";
+    clickMe.style.color = toggler ? "white" : "black";
+    clickMe.style.opacity = toggler ? 1 : 0;
+    a.forEach(ele => {
+        ele.style.color = toggler ? "white" : "black";
+        ele.style.opacity = 1
+    });
+    activeSection.style.opacity = toggler ? 1 : 0; 
+    activeLink.style.color =  "black" ;
+
+    toggler = !toggler;
+});
+
+aboutLink.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if(toggler) nameBanner.click();
+    selectedOpacity(about);
+    selectedLinkColor(aboutLink);
+});
+
+projectsLink.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if(toggler) nameBanner.click();
+    selectedOpacity(projects);
+    selectedLinkColor(projectsLink)
+});
+
+resumeLink.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if(toggler) nameBanner.click();
+    selectedOpacity(resume);
+    selectedLinkColor(resumeLink);
+});
